3ba86bf2838528c784935f99dbd0b0746eb64080
Emmanuel <enobakhare.emmanuel@gmail.com>
2021-10-26 14:53:48 +0100

    updated...

--- src/components/Auth/GuestGuard.js
+++ src/components/Auth/GuestGuard.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { BASE_URL } from '../../config/constant';
+import React from "react";
+// import { Redirect } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { BASE_URL } from '../../config/constant';
 
 const GuestGuard = ({ children }) => {
-    const account = useSelector((state) => state.account);
-    const { isLoggedIn } = account;
+  // const account = useSelector((state) => state.account);
+  // const { isLoggedIn } = account;
 
-    // if (isLoggedIn) {
-    //     return <Redirect to={BASE_URL} />;
-    // }
+  // if (isLoggedIn) {
+  //     return <Redirect to={BASE_URL} />;
+  // }
 
-    return <React.Fragment>{children}</React.Fragment>;
+  return <React.Fragment>{children}</React.Fragment>;
 };
 
 export default GuestGuard;
